154225883182f2977942407e8b48ff0eda006d4e
Ahmedtijani Umar <whizmoney@yahoo.com>
2021-01-30 14:38:28 +0100

    fixed PORT

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ app.use((err, req, res, next) => {
   }
 });
 
-app.listen(process.env.PORT || 8080, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
